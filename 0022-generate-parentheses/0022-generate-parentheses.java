7e26b3324b5bc2c97c9061e7780cb08484757f7c
Fazlul Ajmal <fazlulajmal@gmail.com>
2025-09-18 09:43:34 +0530

    Time: 1 ms (81.16%), Space: 42.7 MB (94.29%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder("");
+        paranThesis(sb,n,0,0,ans);
+        return ans;
+    }
+    static void paranThesis(StringBuilder sb,int n, int open, int close, List<String> ans){
+        if (close ==n){
+            ans.add(sb.toString());
+            return;
+        }
+        else{
+            if(open < n){
+                paranThesis(sb.append("("),n,open+1,close,ans);
+                sb.deleteCharAt(sb.length()-1);
+            }
+            if(close < open){
+                paranThesis(sb.append(")"),n,open,close+1,ans);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+}
