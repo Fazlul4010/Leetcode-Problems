ca639a005db6730a64207f9d8042b756d27dff71
Fazlul Ajmal <fazlulajmal@gmail.com>
2025-10-06 21:25:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int l=0;
+        int mid=0;
+        int h=nums.length-1;
+        while (mid <= h) {
+            if (nums[mid] == 0) {
+                // Swap nums[low] and nums[mid]
+                int temp = nums[l];
+                nums[l] = nums[mid];
+                nums[mid] = temp;
+
+                l++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { 
+                // nums[mid] == 2
+                // Swap nums[mid] and nums[high]
+                int temp = nums[mid];
+                nums[mid] = nums[h];
+                nums[h] = temp;
+
+                h--;
+            }
+        }
     }
 }
